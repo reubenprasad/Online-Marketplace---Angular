f7a56baeca5ee6e3f9074baa1a7f84256cb738e0
reubenprasad <reubenprasad@gmail.com>
2019-09-24 05:26:22 +0530

    cors

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.use(express.static(__dirname + '/dist/finalproject'));
 
